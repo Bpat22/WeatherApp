9ca8b1f7dc17d7d502df0038fe6cd24d8fedbba9
Bpat22 <binapatel0327@gmail.com>
2021-01-17 23:26:16 -0600

    Weather Display

--- src/Card.js
+++ src/Card.js
@@ -1,19 +1,59 @@
 import React from 'react';
 
-// var moment = require('moment');
+var moment = require('moment');
 
-class Card extends React.Component{
-    // Props: day, key(index)
+const DayCard = ({ reading }) => {
+    let newDate = new Date();
+    const weekday = reading.dt * 1000
+    newDate.setTime(weekday)
 
-render (){
+    const imgURL = `owf owf-${reading.weather[0].id} owf-5x`
+
     return (
-        <div>
-            {this.props.day.weather[0].main} 
-        </div>    
-    );
-}    
-
+        <div className="col-sm-2">
+            <div className="card">
+                <h3 className="card-title">{moment(newDate).format('dddd')}</h3>
+                <p className="text-muted">{moment(newDate).format('MMMM Do, h:mm a')}</p>
+                <i className={imgURL}></i>
+                <h2>{Math.round(reading.main.temp)} °F</h2>
+                <div className="card-body">
+                <p className="card-text">{reading.weather[0].description}</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default Card;
+
+// class Card extends React.Component{
+//     // Props: day, key(index)
+
+// render (){
+//     let newDate = new Date();
+//     const weekday = this.props.day.dt * 1000
+//     newDate.setTime(weekday)
+
+//     const imgURL = "owf owf-"+ this.props.day.weather[0].id +" owf-5x red"
+
+//     const farenheit = (parseInt(this.props.day.main.temp) - 273.15) * (9/5) + 32
+//     return (
+//         <div className="col-auto">
+//             <div className="card bg-ligt">
+//                 <h3 className="card-title">{moment(newDate).format('dddd')}</h3>
+//                 <p className="text-muted">{moment(newDate).format('MMMM Do, h:mm a')}</p>
+//                 <i className={imgURL}></i>
+//                 <h2>{Math.floor(farenheit)} F</h2>
+//                 <div className="card-body">
+//                     <p className="card-text"> {this.props.day.weather[0].description}</p>
+//                     <button className="btn btn-dark btn-outline-light">See Hourly Forecast</button>
+//                 </div>
+//             </div>
+//             {/* {this.props.day.weather[0].main}  */}
+//         </div>    
+//         )
+//     }    
+
+// }
+
+export default DayCard;
 
--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -1,35 +1,63 @@
 import React from 'react';
 import apiConfig from './apiKeys';
+import DayCard from './Card';
 
-class WeekContainer extends React.Component {
-    state = {
-        fullData: [],
-        dailyData: []
-    }
+    class WeekContainer extends React.Component {
+        state = {
+            fullData: [],
+            dailyData: []
+        }
+        componentDidMount = () => {
+            const weatherURL =
+            `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${apiConfig.myKey}`
 
-    componentDidMount = () => {
-        const weatherURL =
-        `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${apiConfig.myKey}`
-
-        fetch(weatherURL)
-        .then(res => res.json())
-        .then(data => {
-            const dailyData = data.list.filter(reading => {
-                return reading.dt_txt.includes("18:00:00")})
+            fetch(weatherURL)
+            .then(res => res.json())
+            .then(data => {
+            const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
             this.setState({
                 fullData: data.list,
                 dailyData: dailyData
-        }, () => console.log(this.state))
-    })
-}
+            }, () => console.log(this.state))
+        })
+    }
 
+    formatDayCards = () => {
+        return this.state.dailyData.map((reading, index) => <DayCard reading={reading} key={index} />)
+    }    
     render() {
         return (
-            <div>
-                <h1>Hello World!</h1>
-        </div>
+            <div className="container">
+                <h1 className="display-1 jumbotron">5-Day Forecast.</h1>
+                <h4 className="display-4 text-muted">Dallas, US</h4>
+                    <div className="row justify-content-center">
+                        {this.formatDayCards()}
+                    </div>
+            </div>
         )
     }
 }
+
+// const weatherURL =
+//         `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${apiConfig.myKey}`
+
+// class WeekContainer extends React.Component {
+//     state = {
+//         days: []
+//     }
+//     componentDidMount = () => {
+
+//         fetch(weatherURL)
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log("Data list loaded", data.list)
+//             const dailyData = data.list.filter(reading => {
+//             return reading.dt_txt.includes("18:00:00")})
+//             this.setState({ days: dailyData})
+//         })
+//     }
+// formatCards = () => {
+//     return this.state.days.map((day, index) => <Card day={day} key={index}/>)
+// }
 
 export default WeekContainer;
